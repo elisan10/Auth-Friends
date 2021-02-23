05368e9d74750b9294dba8f8d761dd6c91418473
Eli <elize.san10@gmail.com>
2021-02-23 16:25:16 -0600

    App.js  - Routes set up in App for Login and FriendsList components

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,9 +1,42 @@
+import React from "react";
+import { Route, Link, Switch } from "react-router-dom";
+
+import FriendsList from "./components/FriendsList";
+import Login from "./components/Login";
+
+import Loader from "react-loader-spinner";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello I am here</header>
+      <header className="App-header">
+        <h1>Hello I am here</h1>
+        <nav className="nav-bar">
+          <Link className="a" to="/login">
+            Login
+          </Link>
+          <Link className="a" to="/friends">
+            Friends
+          </Link>
+        </nav>
+      </header>
+      <div className="app-container">
+        <Loader
+          type="ThreeDots"
+          color="#5e433c"
+          height={80}
+          width={80}
+          timeout={3000}
+        />
+      </div>
+      <div className="Router">
+        <Switch>
+          <Route exact path="/friends" component={FriendsList} />
+          <Route path="/login" component={Login} />
+        </Switch>
+      </div>
     </div>
   );
 }
---
+++ friends/src/components/PrivateRoute.js
@@ -0,0 +1,10 @@
+// import React from 'react'
+// import { Redirect, Route } from "react-router-dom"
+
+// function PrivateRoute() {
+//    return (
+//        <Route
+//    )
+// }
+
+// export default PrivateRoute
